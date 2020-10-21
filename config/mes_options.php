57c919aca452dc8b0bb3405b986c8960191deb35
bordun <jean-emmanuel.doucet@groolot.net>
2020-10-21 14:18:20 +0200

    update config & mobile fix

--- config/mes_options.php
+++ config/mes_options.php
@@ -1,11 +1,6 @@
 <?php
 
-    define('_NO_CACHE', -1); 
+    define('_NO_CACHE', -1);
 
-    $GLOBALS['spip_pipeline']['affichage_final'] .= "|retirer_saisies";
-    function retirer_saisies($flux){
 
-        return preg_replace('/<!--!inserer_saisie_editer-->/', '', $flux);
-
-    }
 ?>
